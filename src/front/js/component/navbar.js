74e028624eead0464a2f903918d18242cbe706ce
Raul Madero <127880270+Raul-madero@users.noreply.github.com>
2024-01-29 18:08:46 +0000

    Registro y login

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -29,6 +29,9 @@ export const Navbar = () => {
 					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
 				</Link>
 				{renderLink()}
+				<Link to="/signup">
+					<button className="btn btn-primary">Registrar</button>
+				</Link>
 				<div className="ml-auto">
 					<Link to="/demo">
 						<button className="btn btn-primary">Check the Context in action</button>
